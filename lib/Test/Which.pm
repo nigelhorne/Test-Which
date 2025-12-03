3bed86050b1a972d94bb513a33d5fd608f3b9c1c
Nigel Horne <njh@bandsman.co.uk>
2025-12-03 08:17:20 -0500

    Release 0.05

--- lib/Test/Which.pm
+++ lib/Test/Which.pm
@@ -23,11 +23,11 @@ Test::Which - Skip tests if external programs are missing from PATH (with versio
 
 =head1 VERSION
 
-Version 0.04
+Version 0.05
 
 =cut
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 =head1 SYNOPSIS
 
